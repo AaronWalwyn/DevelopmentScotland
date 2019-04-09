651a6d38e75cc8b7ede7799408c8c50b6bc401cb
Aaron Walwyn <aaronwalwyn@btinternet.com>
2019-04-09 23:40:28 +0100

    Added more development data

--- js/main.js
+++ js/main.js
@@ -1,35 +1,28 @@
+var _devPoly;
+var _devPolyCreated = false;
+
 var _map = Initialise();
-//AddPoly();
 LoadDevelopmentData();
+__SetupDevFeatures();
 
 function Initialise()
 {
     var map = L.map( 'map', {
         center: [55.948513, -3.199966],
         minZoom:10,
-        zoom: 12
+        zoom: 12,
+
+        attributionControl: false
     });
     
     L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        /*attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',*/
         subdomains: ['a','b','c']
     }).addTo( map );
 
     L.tileLayer.provider('OpenStreetMap.HOT').addTo(map);
 
     return map;
-}
-
-function AddPoly()
-{
-    var polygon = L.polygon([
-        [55.946307,-3.1929159],
-        [55.9456527,-3.2043714],
-        [55.9510052,-3.2058432],
-        [55.9507843,-3.1940235]
-    ]).addTo( _map );
-
-    polygon.bindPopup("I am a polygon.");
 }
 
 function LoadDevelopmentData()
@@ -42,29 +35,68 @@ function __LoadDevelopmentData(data)
     var count = data.developments.length;
     console.log("Count: " + count);
 
+    var latDif = 55.9642874 - 55.9622216;
+    var longDif = -3.2186344 - -3.2226714
+
     for(var i = 0; i < count; i++)
     {
         points = data.developments[i].Points;
-        var latlngs = [];
 
-        var s = "";
-
-        for(j = 0; j < points.length; j++)
+        if(points.length == 0)
         {
-            latlngs.push([points[j].lat, points[j].long])
-            s += "{\"lat\" : " + points[j].lat + ", \"long\" : " + points[j].long + "}, \n"
+            points.push({"lat" : 55.9639511 + (i * latDif), "long" : -3.2216407});
+            points.push({"lat" : 55.9612127 + (i * latDif), "long" : -3.2216407 + longDif});
+            points.push({"lat" : 55.9609725 + ((i + 1) * latDif), "long" : -3.2162295});
+            points.push({"lat" : 55.9638550 + ((i + 1) * latDif), "long" : -3.2160577 + longDif});
         }
 
-        console.log(s);
+       var latlngs = [];
+       for(j = 0; j < points.length; j++)
+       {
+           latlngs.push([points[j].lat, points[j].long])
+       }
 
         var polygon = L.polygon(latlngs).addTo( _map );
 
-        var pop = "";
-        pop += data.developments[i].Name + "<br />";
-        pop += "<a href=\"" + data.developments[i].URL + "\" target=\"_blank\">Website</a>"
-        pop += " | "
-        pop += "<a href=\"" + data.developments[i].URL + "\" target=\"_blank\">Skyscraper City</a>"
+        var pop =   "<div class=\"development\">";
+        pop +=      "<div class=\"development-name\">" + data.developments[i].Name + "</div>";
+        pop +=      "<img class=\"development-thumb\" src=\"" + data.developments[i].Image + "\">"
+        pop +=      "<ul>"
+        pop +=      "<li><a href=\"" + data.developments[i].URL + "\" target=\"_blank\">Official Website</a></li>"
+        pop +=      "<li><a href=\"" + data.developments[i].URL + "\" target=\"_blank\">Skyscraper City</a></li>"
+        pop +=      "</ul>"
+        pop +=      "</div>"
         
         polygon.bindPopup(pop);
     }
+}
+
+function __SetupDevFeatures()
+{
+    _map.on('click', function(e) {
+
+        if(_devPolyCreated)
+        {
+            _devPoly.addLatLng(e.latlng);
+
+            var s = "";
+            var latlngs = _devPoly.getLatLngs();
+            for(var i = 0; i < latlngs.length; i++)
+            {
+                s += '{"lat" : ' + latlngs[i].lat.toFixed(7) + ', "long" : ' + latlngs[i].lng.toFixed(7) + '}';
+
+                if(i < latlngs.length - 1)
+                {
+                    s += ',\n';
+                }
+            }
+            
+            console.log(s);
+        }
+        else
+        {
+            _devPoly = L.polygon([e.latlng]).addTo( _map );
+            _devPolyCreated = true;
+        }              
+    });
 }
